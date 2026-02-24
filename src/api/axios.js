2a074cb2396171929db462b1363fb1c37d590dc5
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-02-24 10:25:47 +0430

    Add Axios API integration with Laravel Sanctum

---
+++ src/api/axios.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+// Create axios instance with base URL
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  withCredentials: true,
+});
+
+// Request interceptor to add auth token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor to handle errors globally
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+// Export the configured axios instance
+export default api;
+
+// Export common HTTP methods for direct use
+export const get = (url, config = {}) => api.get(url, config);
+export const post = (url, data = {}, config = {}) => api.post(url, data, config);
+export const put = (url, data = {}, config = {}) => api.put(url, data, config);
+export const patch = (url, data = {}, config = {}) => api.patch(url, data, config);
+export const del = (url, config = {}) => api.delete(url, config);

57075f5aeb45a3aa5f4e1ed020970a2036d3c075
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-02-24 10:07:32 +0430

    the style is updated a simple text in home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ function App() {
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-lg">
         <h1 className="text-3xl font-bold text-blue-600 mb-4">
-          Wifaq Frontend
+          Wifaq Schools
         </h1>
         <p className="text-gray-700">
-          React + Vite + Tailwind CSS
+          The Main Page  <span className="text-yellow-800">Golden Chance for Learnign </span>
         </p>
       </div>
     </div>

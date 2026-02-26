843563a48922b7107d47d96d7221bc8e846ba43d
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-02-26 11:49:24 +0430

    updated

--- src/pages/hr/Staff.jsx
+++ src/pages/hr/Staff.jsx
@@ -27,7 +27,7 @@ const Icons = {
   ),
   EditStatus: () => (
     <svg className="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
   ),
 };

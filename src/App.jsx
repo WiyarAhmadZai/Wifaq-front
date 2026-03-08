372bfde2dd40a45e4a4c1b3a7798624499895371
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-03-08 16:14:01 +0430

    feat(teacher): add TeachersShow component and routing for teacher detail view
    - Imported TeachersShow component in App.jsx
    - Replaced Placeholder with TeachersShow for teacher management route
    - Replaced Placeholder with TeachersShow for class management teacher route
    - Enabled detailed teacher view for both management sections

--- src/App.jsx
+++ src/App.jsx
@@ -77,6 +77,7 @@ import AnnualPlans from "./pages/teacher-management/AnnualPlans";
 import LessonPlans from "./pages/teacher-management/LessonPlans";
 import Teachers from "./pages/teacher-management/teacher/Teachers";
 import TeachersForm from "./pages/teacher-management/teacher/TeachersForm";
+import TeachersShow from "./pages/teacher-management/teacher/TeachersShow";
 
 // Branches Pages
 import Branches from "./pages/branches/Branches";
@@ -201,7 +202,7 @@ function App() {
           />
           <Route
             path="teacher-management/teachers/show/:id"
-            element={<Placeholder title="View Teacher" />}
+            element={<TeachersShow />}
           />
 
           {/* Class Management Routes - Redirect to Teacher Management */}
@@ -219,7 +220,7 @@ function App() {
           />
           <Route
             path="class-management/teachers/show/:id"
-            element={<Placeholder title="View Teacher" />}
+            element={<TeachersShow />}
           />
 
           {/* Branches Routes */}
